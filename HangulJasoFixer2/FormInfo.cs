d156080e8c2db581c8d4ab9169540235404c1df2
NAMHO KIM <nhkimnamo@gmail.com>
2019-02-17 17:30:31 +0900

    add FormInfo

---
+++ HangulJasoFixer2/FormInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HangulJasoFixer2
+{
+    public partial class FormInfo : Form
+    {
+        public FormInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void LinkLabelBlog_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Specify that the link was visited.
+            this.linkLabelBlog.LinkVisited = true;
+
+            // Navigate to a URL.
+            System.Diagnostics.Process.Start("https://namocom.tistory.com/630");
+        }
+
+        private void ButtonOk_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
